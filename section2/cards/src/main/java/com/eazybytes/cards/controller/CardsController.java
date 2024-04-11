22af23267e55cedc92e1ba544b96767b28438fb4
Harshad Panmand <panmandharshaddev@gmail.com>
2024-04-11 16:58:52 +0530

    Added CREATE, GET REST APIs for cards

---
+++ section2/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -0,0 +1,87 @@
+package com.eazybytes.cards.controller;
+
+import com.eazybytes.cards.constants.CardsConstants;
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.dto.ErrorResponseDto;
+import com.eazybytes.cards.dto.ResponseDto;
+import com.eazybytes.cards.service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(
+        name = "CRUD REST APIs for Cards in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+)
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Validated
+public class CardsController {
+
+    private ICardsService iCardsService;
+
+    @Operation(
+            summary = "Create Card REST API",
+            description = "REST API to create new Card inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PostMapping(value = "/create")
+    public ResponseEntity<ResponseDto> createCard(@Valid @RequestParam String mobileNumber) {
+        iCardsService.createCard(mobileNumber);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
+    }
+
+    @Operation(
+            summary = "Fetch Card Details REST API",
+            description = "REST API to fetch card details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+    @GetMapping(value = "/fetch")
+    public ResponseEntity<CardsDto> fetchCardDetails(@Valid @RequestParam String mobileNumber) {
+        CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cardsDto);
+    }
+
+}
