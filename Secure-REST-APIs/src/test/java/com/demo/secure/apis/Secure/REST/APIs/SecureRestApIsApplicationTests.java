ce7c5876f83269f1d5fff4a6964089ae924ae9e4
Harshad Panmand <panmandharshaddev@gmail.com>
2024-06-04 19:13:37 +0530

    Based code for secure rest apis

---
+++ Secure-REST-APIs/src/test/java/com/demo/secure/apis/Secure/REST/APIs/SecureRestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.secure.apis.Secure.REST.APIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureRestApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
